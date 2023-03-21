53c6c354b588efd7d81ab2ae9bacc98d32ee97e1
Ismoil Sharifov <sharipovismoil01@gmail.com>
2023-03-21 10:46:44 +0500

    Routin was added to Left Menu

--- src/Menu.js
+++ src/Menu.js
@@ -7,90 +7,135 @@ import {
   MoneyCollectOutlined,
   EditOutlined,
   SwitcherOutlined,
-  LogoutOutlined,
+  PoweroffOutlined,
   ControlOutlined,
   TableOutlined,
 } from "@ant-design/icons";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
-const Menu = () => {
+export default function Menu() {
+  const navigate = useNavigate();
+  const curPath = window.location.pathname;
+
   return (
-    <Space>
-      <AMenu
-        mode="inline"
-        defaultOpenKeys={["dashboard"]}
-        onClick={(info) => {
-          console.log(info.key);
-        }}
-        items={[
-          {
-            label: <Input.Search allowClear placeholder="Search menu items" />,
-            key: "search",
-          },
-          {
-            label: "Home",
-            key: "home",
-            icon: <HomeOutlined />,
-          },
-          {
-            label: "Dashboard",
-            key: "dashboard",
-            icon: <DashboardOutlined />,
-            children: [
-              {
-                label: "Revenue",
-                key: "revenue",
-                icon: <DollarCircleOutlined />,
-              },
-              {
-                label: <span style={{ color: "red" }}>Expenses</span>,
-                key: "expenses",
-                icon: <MoneyCollectOutlined />,
-              },
-            ],
-          },
-          {
-            label: "User Management",
-            key: "um",
-            children: [
-              {
-                label: "Edit Profile",
-                key: "ep",
-                icon: <EditOutlined />,
-              },
-              {
-                label: "Switch Account",
-                key: "sa",
-                icon: <SwitcherOutlined />,
-              },
-            ],
-          },
-          {
-            label: "Admin",
-            key: "ad",
-            type: "group",
-            children: [
-              {
-                label: "Admin Dashboard",
-                key: "add",
-                icon: <ControlOutlined />,
-              },
-              {
-                label: "Challenges",
-                key: "chll",
-                icon: <TableOutlined />,
-              },
-            ],
-          },
-          {
-            label: "Sign-out",
-            key: "sign-out",
-            danger: true,
-            icon: <LogoutOutlined />,
-          },
-        ]}
-      ></AMenu>
-    </Space>
+    <>
+      <h5>Menu contains Routing!</h5>
+      <Space>
+        <AMenu
+          mode="inline"
+          defaultOpenKeys={[curPath]}
+          onClick={({ key }) => {
+            if (
+              key === "/dashboard" ||
+              key === "/user-management" ||
+              key === "/admin"
+            ) {
+              console.log(key);
+            } else {
+              navigate(key);
+            }
+          }}
+          items={[
+            {
+              label: (
+                <Input.Search allowClear placeholder="Search menu items" />
+              ),
+              key: "search",
+            },
+            {
+              label: "Home",
+              key: "/",
+              icon: <HomeOutlined />,
+            },
+            {
+              label: "Dashboard",
+              key: "/dashboard",
+              icon: <DashboardOutlined />,
+              children: [
+                {
+                  label: "Revenue",
+                  key: "/revenue",
+                  icon: <DollarCircleOutlined />,
+                },
+                {
+                  label: <span style={{ color: "red" }}>Expenses</span>,
+                  key: "/expenses",
+                  icon: <MoneyCollectOutlined />,
+                },
+              ],
+            },
+            {
+              label: "User Management",
+              key: "/user-management",
+              children: [
+                {
+                  label: "Edit Profile",
+                  key: "/adit-profile",
+                  icon: <EditOutlined />,
+                },
+                {
+                  label: "Switch Account",
+                  key: "/switch-account",
+                  icon: <SwitcherOutlined />,
+                },
+              ],
+            },
+            {
+              label: "Admin",
+              key: "/admin",
+              type: "group",
+              children: [
+                {
+                  label: "Admin Dashboard",
+                  key: "/admin-dashboard",
+                  icon: <ControlOutlined />,
+                },
+                {
+                  label: "Challenges",
+                  key: "/challenges",
+                  icon: <TableOutlined />,
+                },
+              ],
+            },
+            {
+              label: "Sign-out",
+              key: "/sign-out",
+              danger: true,
+              icon: <PoweroffOutlined />,
+            },
+          ]}
+        ></AMenu>
+        <Content />
+      </Space>
+    </>
+  );
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" /* element={<App />} */ />;
+        <Route path="/revenue" element={<div>Revenue Page</div>} />;
+        <Route path="/expenses" element={<div>Expenses Page</div>} />;
+        <Route path="/adit-profile" element={<div>Edit Profile Page</div>} />;
+        <Route
+          path="/switch-account"
+          element={<div>Switch Account Page</div>}
+        />
+        ;
+        <Route
+          path="/admin-dashboard"
+          element={<div>Admin Dashboard Page</div>}
+        />
+        ;
+        <Route path="/challenges" element={<div>Challenges Page</div>} />;
+        <Route path="/sign-out" element={<SignOut />} />;
+      </Routes>
+    </div>
   );
 };
 
-export default Menu;
+const SignOut = () => {
+  return <div>This is the Sign-Out Page!</div>;
+};
